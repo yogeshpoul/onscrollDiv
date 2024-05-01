6dfa309c0162392241287679023e415a15677de9
Yogesh Poul <yogeshpoul9999@gmail.com>
2024-05-01 10:26:29 +0530

    fixed navbar is added

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,37 @@ function App() {
 
   return (
     <div>
+      <div className='fixed bg-gray-200 w-full '>
+
+      
       <nav>
-        <ul>
-          <li>
+        <ul className='relative flex flex-col justify-start md:flex-row '>
+          <li className='px-3'>
             <button onClick={() => scrollToSection('home')}>Home</button>
           </li>
-          <li>
+          <li className='px-3'>
             <button onClick={() => scrollToSection('achievements')}>Achievements</button>
           </li>
-          <li>
+          <li className='px-3'>
             <button onClick={() => scrollToSection('complaints')}>Complaints</button>
           </li>
-          <li>
+          <li className='px-3'>
             <button onClick={() => scrollToSection('about-us')}>About Us</button>
           </li>
         </ul>
       </nav>
-      <div id="home" className='bg-red-500 h-screen'>
+      </div>
+      <div id="home" className='bg-red-500 h-screen pt-5 '>
         <div className='text-center'>Home</div>
       </div>
-      <div id="achievements" className='bg-green-500 h-screen text-center'>
+      <div id="achievements" className='bg-green-500 h-screen text-center pt-5'>
         Achievements
       </div>
-      <div id="complaints" className='bg-blue-500 h-screen text-center'>
+      <div id="complaints" className='bg-blue-500 h-screen text-center pt-5'>
         Complaints
         <Complaints/>
       </div>
-      <div id="about-us" className='bg-gray-500 h-screen text-center'>
+      <div id="about-us" className='bg-gray-500 h-screen text-center pt-5'>
         About Us
         
       </div>
